4acda9490886e037564361907cbd87ff6a377615
JuanSMartinez <js.martinez777@uniandes.edu.co>
2018-03-28 22:59:05 -0400

    detecting swiping gestures

--- app/src/main/java/com/eslbuddy/juanmartinez/eslbuddy/MainActivity.java
+++ app/src/main/java/com/eslbuddy/juanmartinez/eslbuddy/MainActivity.java
@@ -1,23 +1,28 @@
 package com.eslbuddy.juanmartinez.eslbuddy;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.wear.widget.BoxInsetLayout;
 import android.support.wearable.activity.WearableActivity;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends WearableActivity{
-
-    //Speech recognition request code
-    public final static int SPEECH_RECOGNIZTION_REQUEST_CODE = 8080;
 
     //Recording permission code
     public final static int SPEECH_RECORDING_PERMISSION = 5000;
 
+    //Swipe threshold
+    public final static int SWIPE_THRESHOLD = 100;
+
+    //coordinates to detect swipe
+    private float x1,y1,x2,y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +42,45 @@ public class MainActivity extends WearableActivity{
             }
         });
 
+        setTouchEventListener();
+
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setTouchEventListener() {
+        BoxInsetLayout layout = findViewById(R.id.mainLayout);
+        layout.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                int action = event.getActionMasked();
+                switch (action){
+                    case MotionEvent.ACTION_DOWN:
+                        x1 = event.getX();
+                        y1 = event.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        x2 = event.getX();
+                        y2 = event.getY();
+                        float deltaX = x1-x2;
+                        float deltaY = y1-y2;
+                        if(deltaX >= SWIPE_THRESHOLD && deltaY < SWIPE_THRESHOLD){
+                            Toast.makeText(getApplicationContext(),"Swipe left",Toast.LENGTH_SHORT).show();
+                        }
+                        else if(deltaX < SWIPE_THRESHOLD && deltaY > SWIPE_THRESHOLD){
+                            Toast.makeText(getApplicationContext(),"Swipe up",Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                return v.performClick();
+            }
+        });
     }
 
     @Override
@@ -48,13 +90,11 @@ public class MainActivity extends WearableActivity{
     }
 
 
+
+
     private boolean checkRecordingPermission(){
         int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO);
-        if (result == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
+        return result == PackageManager.PERMISSION_GRANTED;
     }
 
     private void requestRecordingPermission(){
@@ -63,5 +103,6 @@ public class MainActivity extends WearableActivity{
                     SPEECH_RECORDING_PERMISSION);
         }
     }
+
 
 }
