64e8b59a114756fdda8fbdd1c08bc43b21d69eb7
JuanSMartinez <js.martinez777@uniandes.edu.co>
2018-04-10 23:03:31 -0400

    added on click listeners to menu buttons

--- app/src/main/java/com/eslbuddy/juanmartinez/eslbuddy/MenuActivity.java
+++ app/src/main/java/com/eslbuddy/juanmartinez/eslbuddy/MenuActivity.java
@@ -2,20 +2,45 @@ package com.eslbuddy.juanmartinez.eslbuddy;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MenuActivity extends WearableActivity {
 
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        //Set button listeners
+        setReviewButtonListener();
+        setQuizButtonListener();
+        setQuizToBuddyButtonListener();
+        setSettingsListener();
+
 
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    private void setSettingsListener() {
+        ImageButton button = findViewById(R.id.settings);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void setQuizToBuddyButtonListener() {
+    }
+
+    private void setQuizButtonListener() {
+    }
+
+    private void setReviewButtonListener() {
     }
 }
